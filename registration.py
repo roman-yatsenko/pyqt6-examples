9af927eaf8c24a92fa6a66e363688d2d7fd6344a
YaRo <roman.yatsenko@hneu.net>
2024-03-09 16:25:09 +0200

    Add confirmSignUp

--- registration.py
+++ registration.py
@@ -30,7 +30,7 @@ class NewUserDialog(QDialog):
     def setUpWindow(self):
         login_label = QLabel("Створити новий акаунт", self)
         login_label.setFont(QFont("Arial", 20))
-        login_label.move(90, 20)
+        login_label.move(50, 20)
 
         user_image = "images/new_user_icon.png"
 
@@ -43,7 +43,7 @@ class NewUserDialog(QDialog):
         except FileNotFoundError as error:
             print(f"Image not found.\nError: {error}")
 
-        name_label = QLabel("Ім'я користувача:", self)
+        name_label = QLabel("логін:", self)
         name_label.move(20, 144)
 
         self.name_edit = QLineEdit(self)
@@ -65,22 +65,41 @@ class NewUserDialog(QDialog):
         self.new_passwd_edit.move(90, 200)
         self.new_passwd_edit.setEchoMode(QLineEdit.EchoMode.Password)
 
-        confirm_label = QLabel("Підтвердження:", self)
+        confirm_label = QLabel("Пароль (2):", self)
         confirm_label.move(20, 234)
 
         self.confirm_edit = QLineEdit(self)
         self.confirm_edit.resize(250, 24)
         self.confirm_edit.move(90, 230)
-        self.new_passwd_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.confirm_edit.setEchoMode(QLineEdit.EchoMode.Password)
 
         sign_up_button = QPushButton("Реєстрація", self)
         sign_up_button.resize(320, 32)
         sign_up_button.move(20, 270)
-        # sign_up_button.clicked.connect(self.confirmSignUp)
+        sign_up_button.clicked.connect(self.confirmSignUp)
 
+    def confirmSignUp(self):
+        name_text = self.name_edit.text()
+        pswd_text = self.new_passwd_edit.text()
+        confirm_text = self.confirm_edit.text()
 
-# Запуск програми
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    sys.exit(app.exec())
+        if name_text == "" or pswd_text == "":
+            QMessageBox.warning(
+                self,
+                "Помилка",
+                "Будь ласка, введіть ім'я користувача та пароль",
+                QMessageBox.StandardButton.Close,
+                QMessageBox.StandardButton.Close,
+            )
+        elif pswd_text != confirm_text:
+            QMessageBox.warning(
+                self,
+                "Помилка",
+                "Введені паролі не співпадають",
+                QMessageBox.StandardButton.Close,
+                QMessageBox.StandardButton.Close,
+            )
+        else:
+            with open("files/users.txt", "a", encoding="UTF8") as f:
+                f.write("\n" + name_text + " " + pswd_text)
+            self.close()
