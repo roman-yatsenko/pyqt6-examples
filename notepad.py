08da40cfe1d40e6542e09c686dd02935c55eceec
YaRo <roman.yatsenko@hneu.net>
2024-03-30 16:22:25 +0200

    Add File menu

--- notepad.py
+++ notepad.py
@@ -37,15 +37,32 @@ class MainWindow(QMainWindow):
 
     def createActions(self):
         """Створення дій"""
-        self.quit_act = QAction("&Вихід")
+        self.quit_act = QAction(QIcon("images/exit.png"), "&Вихід")
         self.quit_act.setShortcut("Ctrl+Q")
         self.quit_act.triggered.connect(self.close)
+
+        self.new_act = QAction(QIcon("images/new_file.png"), "Новий")
+        self.new_act.setShortcut("Ctrl+N")
+        # self.new_act.triggered.connect(self.clearText)
+
+        self.open_act = QAction(QIcon("images/open_file.png"), "Відкрити")
+        self.open_act.setShortcut("Ctrl+O")
+        # self.open_act.triggered.connect(self.openFile)
+
+        self.save_act = QAction(QIcon("images/save_file.png"), "Зберегти")
+        self.save_act.setShortcut("Ctrl+S")
+        # self.save_act.triggered.connect(self.saveToFile)
 
     def createMenu(self):
         """Налаштування меню"""
         self.menuBar().setNativeMenuBar(False)
 
         file_menu = self.menuBar().addMenu("&Файл")
+        file_menu.addAction(self.new_act)
+        file_menu.addSeparator()
+        file_menu.addAction(self.open_act)
+        file_menu.addAction(self.save_act)
+        file_menu.addSeparator()
         file_menu.addAction(self.quit_act)
 
 
