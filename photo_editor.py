29234854f4dd80772de5cd7589af9313c3364a03
YaRo <roman.yatsenko@hneu.net>
2024-04-13 14:20:33 +0300

    Add actions & menu for photo editor

---
+++ photo_editor.py
@@ -0,0 +1,147 @@
+import sys
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QDockWidget,
+    QDialog,
+    QMessageBox,
+    QToolBar,
+    QStatusBar,
+    QVBoxLayout,
+)
+from PyQt6.QtCore import Qt, QSize, QRect
+from PyQt6.QtGui import QAction, QIcon, QPixmap, QTransform, QPainter
+
+
+class MainWindow(QMainWindow):
+    """Клас для головного вікна"""
+
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        """Налаштування застосунку"""
+        self.setFixedSize(650, 650)
+        self.setWindowTitle("Photo Editor")
+        self.setUpMainWindow()
+        self.createToolsDockWidget()
+        self.createActions()
+        self.createMenu()
+        self.createToolBar()
+        self.show()
+
+    def setUpMainWindow(self):
+        self.image = QPixmap()
+
+        self.image_label = QLabel()
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(self.image_label)
+
+        self.setStatusBar(QStatusBar())
+
+    def createToolsDockWidget(self):
+        pass
+
+    def createActions(self):
+        """Створення дій"""
+        self.quit_act = QAction("&Вихід")
+        self.quit_act.setShortcut("Ctrl+Q")
+        self.quit_act.setStatusTip("Вихід із програми")
+        self.quit_act.triggered.connect(self.close)
+
+        self.open_act = QAction(QIcon("images/open_file.png"), "Відкрити")
+        self.open_act.setShortcut("Ctrl+O")
+        self.open_act.setStatusTip("Відкрити нове зображення")
+        # self.open_act.triggered.connect(self.openImage)
+
+        self.save_act = QAction(QIcon("images/save_file.png"), "Зберегти")
+        self.save_act.setShortcut("Ctrl+S")
+        self.save_act.setStatusTip("Зберегти зображення")
+        # self.open_act.triggered.connect(self.saveImage)
+
+        self.print_act = QAction(QIcon("images/print.png"), "Друк")
+        self.print_act.setShortcut("Ctrl+P")
+        self.print_act.setStatusTip("Надрукувати зображення")
+        # self.print_act.triggered.connect(self.printImage)
+        self.print_act.setEnabled(False)
+
+        self.rotate90_act = QAction("Повернути на 90°")
+        self.rotate90_act.setStatusTip(
+            "Повернути зображення на 90° за годиниковою стрілкою"
+        )
+        # self.rotate90_act.triggered.connect(self.rotateImage90)
+
+        self.rotate180_act = QAction("Повернути на 180°")
+        self.rotate180_act.setStatusTip(
+            "Повернути зображення на 180° за годиниковою стрілкою"
+        )
+        # self.rotate180_act.triggered.connect(self.rotateImage180)
+
+        self.hor_act = QAction("Перевернути горизонтально")
+        self.hor_act.setStatusTip("Перевернути зображення за горизонтальною віссю")
+        # self.hor_act.triggered.connect(self.flipImageHorizontal)
+
+        self.ver_act = QAction("Перевернути вертикально")
+        self.ver_act.setStatusTip("Перевернути зображення за вертикальною віссю")
+        # self.ver_act.triggered.connect(self.flipImageVertical)
+
+        self.resize_act = QAction("Зменшити розмір вдвічі")
+        self.resize_act.setStatusTip("Зменшення розміру зображення вдвічі")
+        # self.resize_act.triggered.connect(self.resizeImageHalf)
+
+        self.clear_act = QAction(QIcon("images/clear.png"), "Очистити зображення")
+        self.clear_act.setStatusTip("Очистити поточне зображення")
+        self.clear_act.setShortcut("Ctrl+D")
+        # self.clear_act.triggered.connect(self.clearImage)
+
+    def createMenu(self):
+        """Налаштування меню"""
+        self.menuBar().setNativeMenuBar(False)
+
+        file_menu = self.menuBar().addMenu("&Файл")
+        file_menu.addAction(self.open_act)
+        file_menu.addAction(self.save_act)
+        file_menu.addSeparator()
+        file_menu.addAction(self.print_act)
+        file_menu.addSeparator()
+        file_menu.addAction(self.quit_act)
+
+        edit_menu = self.menuBar().addMenu("Редагувати")
+        edit_menu.addAction(self.rotate90_act)
+        edit_menu.addAction(self.rotate180_act)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.hor_act)
+        edit_menu.addAction(self.ver_act)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.resize_act)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.clear_act)
+
+        view_menu = self.menuBar().addMenu("Перегляд")
+        # view_menu.addAction(self.toggle_dock_act)
+
+    def createToolBar(self):
+        tool_bar = QToolBar("Панель інструментів фоторедактора")
+        tool_bar.setIconSize(QSize(24, 24))
+        self.addToolBar(tool_bar)
+
+        tool_bar.addAction(self.open_act)
+        tool_bar.addAction(self.save_act)
+        tool_bar.addAction(self.print_act)
+        tool_bar.addAction(self.clear_act)
+        tool_bar.addSeparator()
+        tool_bar.addAction(self.quit_act)
+
+
+# Запуск програми
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.ApplicationAttribute.AA_DontShowIconsInMenus, True)
+    window = MainWindow()
+    sys.exit(app.exec())
